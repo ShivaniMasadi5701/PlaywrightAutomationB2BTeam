59e110831feaa6a17e2ead1a5b8b2aae2f41918d
ShivaniMasadi5701 <shivanipatel.5701@gmail.com>
2026-01-12 11:08:16 +0530

    Added Test Scripts in Products Page & Test class

--- pages/ProductsPage.ts
+++ pages/ProductsPage.ts
@@ -1,32 +1,80 @@
 import { Page, Locator } from "@playwright/test";
 
-export class ProductsPage{
+export class ProductsPage {
 
-    readonly page:Page;
-    readonly btnProducts : Locator;
-    readonly lblAllProducts : Locator;
-    readonly lstProducts : Locator;
+    readonly page: Page;
+    readonly btnProducts: Locator;
+    readonly lblAllProducts: Locator;
+    readonly lstProducts: Locator;
+    readonly btnFirstProduct: Locator;
+    readonly lblFirstProduct: Locator;
+    readonly lblRs:Locator;
+    readonly lblQuantity:Locator;
+    readonly btnAddToCart;
 
 
-    constructor(page:Page){
-        this.page=page;
-        this.btnProducts =page.getByRole('link', { name: 'Products' });
-        this.lblAllProducts=page.getByRole('heading', { name: 'All Products' });
-        this.lstProducts=page.locator("//div[@class='features_items']");
+
+    constructor(page: Page) {
+        this.page = page;
+        this.btnProducts = page.getByRole('link', { name: 'Products' });
+        this.lblAllProducts = page.getByRole('heading', { name: 'All Products' });
+        this.lstProducts = page.locator("//div[@class='features_items']");
+        this.btnFirstProduct = page.locator('a').filter({ hasText: 'View Product' }).first();
+        this.lblFirstProduct = page.locator("(//div[@class='productinfo text-center']//p)[1]");
+        this.lblRs=page.locator("//span[contains(text(),'Rs')]");
+        this.lblQuantity=page.getByLabel('Quantity:');
+        this.btnAddToCart=page.getByRole('button', { name: 'Add to cart' });
+
     }
 
 
-    async clickOnProductsButton(): Promise<void>{
+    async clickOnProductsButton(): Promise<void> {
         await this.btnProducts.click();
     }
 
-    getAllProductsLabel(){
+    getAllProductsLabel(): Locator {
         return this.lblAllProducts;
-
     }
 
-    getProductsList(){
+    getProductsList(): Locator {
         return this.lstProducts;
+    }
+
+    async clickOnFirstProduct(): Promise<void> {
+        await this.btnFirstProduct.click();
+    }
+
+    async getFirstProductLabel(): Promise<string> {
+        return await this.lblFirstProduct.textContent() ?? '';
+    }
+
+    getProduct(): Locator {
+        return this.lblFirstProduct;
+    }
+
+    // ✅ Dynamic locator[accepts the dynamic text]
+    viewproductLabel(productName: string): Locator {
+        return this.page.getByRole('heading', { name: productName });
+    }
+
+    async getViewProductLabel(productName: string): Promise<string> {
+        return await this.viewproductLabel(productName).textContent() ?? '';
+    }
+
+    getProductByName(productName: string): Locator {
+        return this.viewproductLabel(productName);
+    }
+
+    getRupeesLabel():Locator{
+        return this.lblRs;
+    }
+
+    getQuantityLabel() : Locator{
+        return this.lblQuantity;
+    }
+
+    getAddToCartButton() : Locator{
+        return this.btnAddToCart;
     }
 
 }
--- tests/ProductsTest.spec.ts
+++ tests/ProductsTest.spec.ts
@@ -3,7 +3,6 @@ import { ProductsPage } from '../pages/ProductsPage';
 import { RegisterPage } from '../pages/RegisterPage';
 import registerData from '../data/registerData.json';
 
-
 test.describe("Products Page script", () => {
 
     let registerPage: RegisterPage;
@@ -15,7 +14,7 @@ test.describe("Products Page script", () => {
         await registerPage.navigate();
     });
 
-    test("Verify All Products and Product Detail Page", async ({ page }) => {
+    test("Verify All Products and View Product Detail Page", async ({ page }) => {
 
         await expect(page).toHaveTitle(registerData.user.assertions.pageTitle);
         await expect(registerPage.getHomeButton()).toBeVisible();
@@ -23,7 +22,24 @@ test.describe("Products Page script", () => {
         await productsPage.clickOnProductsButton();
 
         await expect(productsPage.getAllProductsLabel()).toHaveText('All Products');
-        await expect(productsPage.getProductsList()).toBeVisible;
+        await expect(productsPage.getProductsList()).toBeVisible();
 
-    })
+        const firstProductLabel = await productsPage.getFirstProductLabel();
+        console.log(`****FirstLabel :${firstProductLabel}`);
+
+        // const productLabel= await productsPage.getProduct().innerText();
+        // console.log("ProductLabel:"+productLabel);
+        
+        await productsPage.clickOnFirstProduct();
+
+        const viewProductLabel = await productsPage.getViewProductLabel(firstProductLabel);
+        console.log("ProductLabel:"+viewProductLabel);
+
+        await expect(productsPage.getProductByName(firstProductLabel)).toBeVisible();
+        expect(firstProductLabel.trim()).toBe(viewProductLabel.trim());
+        await expect(productsPage.getRupeesLabel()).toBeVisible();
+        await expect(productsPage.getAddToCartButton()).toBeVisible();
+
+
+    });
 });
